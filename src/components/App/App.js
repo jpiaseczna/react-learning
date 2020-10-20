39c6486a6d94c6aacc196cf4c414976de31fd81b
Jolanta Piaseczna <piaseczna@onet.pl>
2020-10-20 18:19:34 +0200

    Add columns

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,8 @@ class App extends React.Component {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
+        <h2 className={styles.subtitle}>A simple to-do app with lists, columns and cards</h2>
+        <List title={['Things to do ', <sup>soon!</sup>]} image={'../src/images/space.png'}></List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js'
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  static defaultProps = {
+    children: <p>Interesting things I want to check out!</p>,
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} image={this.props.image} />
+        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+        	<Column columnTitle={'Animals'} />
+        	<Column columnTitle={'Plants'} />
+        	<Column columnTitle={'Minerals'} />
+        </div>
       </section>
-    )
+    );
   }
 }
 
