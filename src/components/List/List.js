b94a72d45722af21998610b9983fa5fbe533e2f5
Jolanta Piaseczna <piaseczna@onet.pl>
2020-10-20 23:04:09 +0200

    Add ReactHtmlParser

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,20 +3,23 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
+	columns: PropTypes.array,
   }
   static defaultProps = {
-    children: <p>Interesting things I want to check out!</p>,
+    description: settings.defaultListDescription,
   }
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
         <div className={styles.columns}>
         	<Column columnTitle={'Animals'} />
         	<Column columnTitle={'Plants'} />
