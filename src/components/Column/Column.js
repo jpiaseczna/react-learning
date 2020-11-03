616c44d3ee10ce18ee488074241c74f11ba64a48
Jolanta Piaseczna <piaseczna@onet.pl>
2020-11-03 23:35:07 +0100

    Add columns and cards adding

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -18,7 +18,7 @@ class Column extends React.Component {
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -32,14 +32,12 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*
         <div className={styles.creator}> 
           <Creator
             text={settings.cardCreatorText}
-            action={(title) => this.addCard(title)}
+            action={addCard}
           />
         </div>
-    	*/}
       </section>
     );
   }
