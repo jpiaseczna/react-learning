688a009716807a701b4d94e62f2a9a699b5d2ec6
Jolanta Piaseczna <piaseczna@onet.pl>
2020-11-22 14:12:10 +0100

    Add Info and FAQ

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -12,6 +12,9 @@ export const settings = {
   },
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
+  header: {
+    icon: 'check-circle',
+  },
 };
 
 export const pageContents = {
@@ -70,6 +73,18 @@ export const listData = {
       ],
     },
   ],
+};
+
+export const InfoData = {
+  title: 'Information',
+  image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+};
+
+export const FAQData = {
+  title: 'Frequently Asked Questions',
+  image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 };
 
 const lists = [
